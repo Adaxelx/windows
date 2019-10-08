9dc2582adaadccef1806b1bc64f33e6f4e32370f
Adrian Mostowski <adaxelx@o2.pl>
2019-10-08 19:59:36 +0200

    Updates

---
+++ precache-manifest.0d0092605ec39aa23e0d33ba21f6ca0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48fd982cdb7aa2bdef6a6490e80db1c",
+    "url": "/windows/index.html"
+  },
+  {
+    "revision": "c049a0021e8dc22f1e62",
+    "url": "/windows/static/js/2.fb11b55f.chunk.js"
+  },
+  {
+    "revision": "2acf06eb73593809099c",
+    "url": "/windows/static/js/main.f9af954f.chunk.js"
+  },
+  {
+    "revision": "4943a02027ba9ae38f99",
+    "url": "/windows/static/js/runtime~main.d7c1f94e.js"
+  },
+  {
+    "revision": "26d15da76f533ba66742757da94001d1",
+    "url": "/windows/static/media/logo2.26d15da7.png"
+  },
+  {
+    "revision": "230c6d1f2a474bae7c3fab62bb9ab4aa",
+    "url": "/windows/static/media/wallpaper.230c6d1f.jpg"
+  }
+]);
